d299b572cc7b9bd2d25f741c0707cac892af2737
Ifeoluwa-Joel <69189931+Ifeoluwa-Joel@users.noreply.github.com>
2023-02-04 16:56:51 +0100

    End of Day 1 Program

---
+++ Day-01/Bandname_Generator.py
@@ -0,0 +1,8 @@
+userName = input("What is your name?\n")
+print("Hi there, " + userName)
+print("Now, let's get you a Band Name!")
+city = input("What City did you grew up in?\n")
+pet = input("What is your pet name?\n")
+
+print("We suggest '" + city + " " + pet +"s' as your Bandname")
+print("What do you think? It rocks!")
