fd3900a94601a7b54aab5f6a695bca77b7ad619c
Ifeoluwa-Joel <69189931+Ifeoluwa-Joel@users.noreply.github.com>
2023-02-06 17:06:26 +0100

    Day 2 - First push

---
+++ Day-02/Primitive-datatypes.py
@@ -0,0 +1,18 @@
+#Data Types
+
+#Strings
+
+print("Hello"[3])
+
+#Integer
+print(123 + 345)
+print(1_000 + 2_000)
+
+# Float
+3.14159
+
+
+# Boolean
+# Only two possible values 'True' or 'False'
+
+
